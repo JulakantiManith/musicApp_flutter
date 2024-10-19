095c5615205cfcb2e94b59529a412b74c2bacf49
2200033020 <2200033020@kluniversity.in>
2024-10-19 12:30:59 +0530

    added to git

---
+++ client/lib/core/constants/server_constant.dart
@@ -0,0 +1,9 @@
+import 'dart:io';
+
+class ServerConstant {
+  static String serverURL =
+      Platform.isAndroid ? 'http://10.0.2.2:8000' : 'http://127.0.0.1:8000';
+}
+// class ServerConstant {
+//   static String serverURL = 'http://127.0.0.1:8000';
+// }
